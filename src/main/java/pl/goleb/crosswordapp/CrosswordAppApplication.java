f1d030ba261b1205f4324157724e0aef8d9ef27d
Wojciech Golebiewski <goleb.wojciech@gmail.com>
2023-02-09 08:01:56 +0100

    create

---
+++ src/main/java/pl/goleb/crosswordapp/CrosswordAppApplication.java
@@ -0,0 +1,13 @@
+package pl.goleb.crosswordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosswordAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrosswordAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/goleb/crosswordapp/CrosswordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.goleb.crosswordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosswordAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
